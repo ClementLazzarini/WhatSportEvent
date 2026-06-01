e98a3f575534df30661ef8b22795707621166588
Clément LAZZARINI <87820286+ClementLazzarini@users.noreply.github.com>
2026-06-01 10:09:15 +0200

    feat: implement dynamic sport filters combined with city search
    Grouped sport radio buttons and city search input into a single form. Used hx-include to pass all form data simultaneously and leveraged Tailwind's peer utility to style custom active states for hidden radio inputs.

--- MatchFinder/views.py
+++ MatchFinder/views.py
@@ -1,23 +1,33 @@
 from django.shortcuts import render
-from .models import Event
+from .models import Event, Sport
 
 def home_view(request):
+    # 1. On écoute la ville ET le sport dans l'URL
     search_city = request.GET.get('city', '')
-    events = Event.objects.select_related('sport').order_by('start_time')
+    search_sport = request.GET.get('sport', '')
 
+    # 2. Requêtes de base
+    events = Event.objects.select_related('sport').order_by('start_time')
+    sports = Sport.objects.all()
+
+    # 3. Application des filtres cumulables
     if search_city:
         events = events.filter(city__icontains=search_city)
+        
+    if search_sport:
+        events = events.filter(sport__slug=search_sport)
 
     events = events[:12]
+    
+    # 4. On passe tout au contexte
     context = {
         'events': events,
-        'search_city': search_city
+        'search_city': search_city,
+        'search_sport': search_sport,
+        'sports': sports,
     }
 
-    # --- MAGIE HTMX ICI ---
-    # Si la requête vient de HTMX, on ne renvoie QUE la grille des matchs
     if request.headers.get('HX-Request'):
         return render(request, 'matchfinder/partials/event_list.html', context)
     
-    # Sinon (premier chargement de la page), on renvoie la page entière
     return render(request, 'matchfinder/index.html', context)
